3da1e88af0a79ed992cc7ef0a9132389f5263e32
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2025-06-23 18:35:49 +0900

    Refactor project structure and add simulation and utility modules

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # mdtbx
 Toolbox for MD simulation
+
+## dependencies
+- Python 3.10
+- Gromacs 2025.1
+- AmberTools 24
+- PyMOL 3.1.0
+- OpenBabel 3.1.0
+
+
+
+
---
+++ src/simulator/sample_run.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+#SBATCH -p all_q
+#SBATCH -n 16
+#SBATCH -J test_md
+#SBATCH -o test_md.out
+#SBATCH -e test_md.err
+#SBATCH --gres=gpu:1
+set -e
+
+source $MODULESHOME/init/bash
+module purge
+module load gromacs/2025.1
+
+restart_gro="gmx.gro"
+MAXWARN=10
+
+# minimization
+i=0
+if [ ! -f "step6.${i}_minimization.gro" ]; then
+    gmx grompp \
+        -f step6.${i}_minimization.mdp \
+        -c ${restart_gro} \
+        -r gmx.gro \
+        -n index.ndx \
+        -p gmx.top \
+        -maxwarn ${MAXWARN} \
+        -o step6.${i}_minimization.tpr
+    gmx mdrun -v -deffnm step6.${i}_minimization
+fi
+restart_gro="step6.${i}_minimization.gro"
+
+# nvt npt
+for i in {1..6};
+do
+    if [ ! -f "step6.${i}_equilibration.gro" ]; then
+        gmx grompp \
+            -f step6.${i}_equilibration.mdp \
+            -c ${restart_gro} \
+            -r gmx.gro \
+            -n index.ndx \
+            -p gmx.top \
+            -maxwarn ${MAXWARN} \
+            -o step6.${i}_equilibration.tpr
+        gmx mdrun -v -deffnm step6.${i}_equilibration
+    fi
+
+    restart_gro="step6.${i}_equilibration.gro"
+done
+
+# production
+for i in {1..10};
+do
+    if [ ! -f "step7_${i}.gro" ]; then
+        # Use restraints option with force=0 for system converted by acpype
+        #   restraints force is 0 during production MD
+        #   restraints is enabled to prevent segmentation fault
+        gmx grompp \
+            -f step7_production.mdp \
+            -c ${restart_gro} \
+            -n index.ndx \
+            -p gmx.top \
+            -maxwarn ${MAXWARN} \
+            -o step7_${i}.tpr
+            # -r gmx.gro \
+        gmx mdrun -v -deffnm step7_${i} -dlb no
+    fi
+    restart_gro="step7_${i}.gro"
+done
+
+echo done
