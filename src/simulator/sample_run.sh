65380ac5ed8a2350ccc029e8f6a327a40514cf51
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2025-06-24 19:35:14 +0900

    Update sample run script.

--- src/simulator/sample_run.sh
+++ src/simulator/sample_run.sh
@@ -11,14 +11,16 @@ source $MODULESHOME/init/bash
 module purge
 module load gromacs/2025.1
 
-restart_gro="gmx.gro"
+GMX_CMD="gmx"
 MAXWARN=10
-gmx_cmd="gmx"
+PRODUCTION_STEPS=10
+
+restart_gro="gmx.gro"
 
 # minimization
 i=0
 if [ ! -f "step${i}_minimization.gro" ]; then
-    $gmx_cmd grompp \
+    $GMX_CMD grompp \
         -f step${i}_minimization.mdp \
         -c ${restart_gro} \
         -r gmx.gro \
@@ -26,7 +28,7 @@ if [ ! -f "step${i}_minimization.gro" ]; then
         -p gmx.top \
         -maxwarn ${MAXWARN} \
         -o step${i}_minimization.tpr
-    $gmx_cmd mdrun -v -deffnm step${i}_minimization
+    $GMX_CMD mdrun -v -deffnm step${i}_minimization
 fi
 restart_gro="step${i}_minimization.gro"
 
@@ -34,7 +36,7 @@ restart_gro="step${i}_minimization.gro"
 for i in {1..6};
 do
     if [ ! -f "step${i}_equilibration.gro" ]; then
-        $gmx_cmd grompp \
+        $GMX_CMD grompp \
             -f step${i}_equilibration.mdp \
             -c ${restart_gro} \
             -r gmx.gro \
@@ -42,19 +44,19 @@ do
             -p gmx.top \
             -maxwarn ${MAXWARN} \
             -o step${i}_equilibration.tpr
-        $gmx_cmd mdrun -v -deffnm step${i}_equilibration
+        $GMX_CMD mdrun -v -deffnm step${i}_equilibration
     fi
     restart_gro="step${i}_equilibration.gro"
 done
 
 # production
-for i in {1..10};
+for i in $(seq 1 ${PRODUCTION_STEPS});
 do
     if [ ! -f "step7_${i}.gro" ]; then
         # Use restraints option with force=0 for system converted by acpype
         #   restraints force is 0 during production MD
         #   restraints is enabled to prevent segmentation fault
-        $gmx_cmd grompp \
+        $GMX_CMD grompp \
             -f step7_production.mdp \
             -c ${restart_gro} \
             -n index.ndx \
@@ -62,7 +64,7 @@ do
             -maxwarn ${MAXWARN} \
             -o step7_${i}.tpr
             # -r gmx.gro \
-        $gmx_cmd mdrun -v -deffnm step7_${i} -dlb no
+        $GMX_CMD mdrun -v -deffnm step7_${i} -dlb no
     fi
     restart_gro="step7_${i}.gro"
 done
