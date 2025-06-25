a067f2339b77baa325f4872d409087e910a37c44
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2025-06-25 14:04:00 +0900

    Rename sample run script to indicate SLURM usage.

--- src/simulator/sample_mdrun_slurm.sh
+++ src/simulator/sample_mdrun_slurm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #SBATCH -p all_q
-#SBATCH -n 16
+#SBATCH -N 1
+#SBATCH --exclusive
 #SBATCH -J test_md
 #SBATCH -o test_md.out
 #SBATCH -e test_md.err
@@ -14,6 +15,11 @@ module load gromacs/2025.1
 GMX_CMD="gmx"
 MAXWARN=10
 PRODUCTION_STEPS=10
+MDRUN_OPTION="-dlb no -nb gpu -pme gpu -pmefft gpu -bonded gpu -update gpu"
+
+echo "hostname: $(hostname)"
+echo "cpus: $SLURM_CPUS_ON_NODE"
+echo "gpus: $SLURM_GPUS_ON_NODE"
 
 restart_gro="gmx.gro"
 
@@ -28,7 +34,7 @@ if [ ! -f "step${i}_minimization.gro" ]; then
         -p gmx.top \
         -maxwarn ${MAXWARN} \
         -o step${i}_minimization.tpr
-    $GMX_CMD mdrun -v -deffnm step${i}_minimization
+    $GMX_CMD mdrun -v -deffnm step${i}_minimization ${MDRUN_OPTION}
 fi
 restart_gro="step${i}_minimization.gro"
 
@@ -44,7 +50,7 @@ do
             -p gmx.top \
             -maxwarn ${MAXWARN} \
             -o step${i}_equilibration.tpr
-        $GMX_CMD mdrun -v -deffnm step${i}_equilibration
+        $GMX_CMD mdrun -v -deffnm step${i}_equilibration ${MDRUN_OPTION}
     fi
     restart_gro="step${i}_equilibration.gro"
 done
@@ -64,9 +70,13 @@ do
             -maxwarn ${MAXWARN} \
             -o step7_${i}.tpr
             # -r gmx.gro \
-        $GMX_CMD mdrun -v -deffnm step7_${i} -dlb no
+        $GMX_CMD mdrun -v -deffnm step7_${i} ${MDRUN_OPTION}
     fi
     restart_gro="step7_${i}.gro"
 done
+
+# remove temporary files
+rm -f *cpt
+rm -f \#*
 
 echo done
