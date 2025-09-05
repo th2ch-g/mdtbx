eab08c62653db72a1b4f7e7d6a6a848249c62b7a
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2025-09-05 15:14:04 +0900

    chore

--- pymol-plugins/pymol_plugins/__init__.py
+++ pymol-plugins/pymol_plugins/__init__.py
@@ -47,7 +47,63 @@ def comdist(selection1, selection2):
 cmd.extend("comdist", comdist)
 
 
-def mydefault():
+def make_trj_movie():
+    cmd.viewport(6400, 5200)
+    cmd.mset("1 -%d" % cmd.count_states())
+    cmd.mpng("traj_frame_")
+    import subprocess
+
+    subprocess.run(
+        "ffmpeg -r 30 -i traj_frame_%04d.png -c:v libx264 -pix_fmt yuv420p out.mp4",
+        shell=True,
+        check=True,
+    )
+    subprocess.run("rm -f traj_frame_*.png", shell=True, check=True)
+    print("movie is saved as out.mp4")
+
+
+cmd.extend("make_trj_movie", make_trj_movie)
+
+
+def laboo_settings():
+    cmd.reinitialize("settings")
+    cmd.set("sphere_scale", 0.22)
+    cmd.set("sphere_scale", 0.22, "elem C+N+O+S+Cl+F+Na+Mg")
+    cmd.set("sphere_scale", 0.13, "elem H")
+    cmd.set("hide_long_bonds", 1)
+    cmd.set("dash_gap", 0)
+    cmd.set("dash_gap", 0.15)
+    cmd.set("dash_length", 0.05)
+    cmd.set("dash_round_ends", 0)
+    cmd.set("dash_radius", 0.05)
+    cmd.set("label_size", 18)
+    cmd.set("cartoon_loop_radius", 0.1)
+    cmd.set("cartoon_putty_radius", 0.2)
+    cmd.set("cartoon_oval_length", 0.8)
+    cmd.set("cartoon_gap_cutoff", 0)
+    cmd.set("cartoon_rect_length", 1.2)
+    cmd.set("label_digits", 3)
+    cmd.set("ray_opaque_background", 0)
+    cmd.set("dash_length", 0.2500)
+    cmd.set("retain_order", 1)
+    cmd.set("pdb_retain_ids", 1)
+    cmd.set("suspend_undo_atom_count", 0)
+    cmd.set("ray_trace_mode", 1)
+    cmd.set("ray_trace_gain", 0.05)
+    cmd.set("ray_trace_color", "gray37")
+    # cmd.bg_color("white")
+    cmd.set("fog", 1)
+    cmd.set("ambient", 0.66)
+    cmd.set("reflect", 0)
+    cmd.set("spec_reflect", 0.08)
+    cmd.set("light_count", 2)
+
+
+cmd.extend("laboo_settings", laboo_settings)
+
+
+def my_settings():
+    cmd.reinitialize("settings")
     # save setting
     cmd.set("pdb_reformat_names_mode", 2)
     cmd.set("retain_order", 1)
@@ -64,5 +120,5 @@ def mydefault():
     cmd.set("ray_trace_color", "black")
 
 
-cmd.extend("mydefault", mydefault)
-mydefault()
+cmd.extend("my_settings", my_settings)
+my_settings()
