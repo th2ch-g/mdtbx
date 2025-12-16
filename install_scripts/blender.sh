840ec2edc0ccd486ea14bf18a165a9d06f795c38
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2025-12-16 14:23:20 +0900

    add

--- install_scripts/blender.sh
+++ install_scripts/blender.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -e
 brew install --cask blender
+URL=https://github.com/BradyAJohnston/MolecularNodes/releases/download/v4.5.9/molecularnodes-4.5.9-macos_arm64.zip
+wget $URL
 echo done
