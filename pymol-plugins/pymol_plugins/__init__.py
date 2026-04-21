a65417ddbd096ddc54e673e649e72b09c9c1892c
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2026-04-21 16:44:54 +0900

    add

--- pymol-plugins/pymol_plugins/__init__.py
+++ pymol-plugins/pymol_plugins/__init__.py
@@ -123,7 +123,7 @@ def my_settings():
     cmd.set("pdb_reformat_names_mode", 2)
     cmd.set("retain_order", 1)
     cmd.set("pdb_retain_ids", 1)
-    cmd.set("pdb_connect_all", "off")
+    # cmd.set("pdb_connect_all", "off")  # unknown setting in current PyMOL version
     # cmd.set("cartoon_highlight_color", "grey50")
 
     # visualization setting
